94c6ab277913bf0648c41618ffd59a335c0e64b1
Abraham Armas Cordero <abraham.armas@enti.cat>
2019-12-10 00:50:50 +0100

    UI Canvas and edificio script

--- Triumph-Wall_Project/Assets/Scripts/UI/UIControlller.cs
+++ Triumph-Wall_Project/Assets/Scripts/UI/UIControlller.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class UIControlller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public void SetUp ( )
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
+
+	public void Tick ( )
+	{
+
+	}
 }
