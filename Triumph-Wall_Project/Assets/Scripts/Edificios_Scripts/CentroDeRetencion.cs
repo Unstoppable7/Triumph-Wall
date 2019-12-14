048f608a8692e55488c244fe999348eda14910af
Abraham Armas Cordero <abraham.armas@enti.cat>
2019-12-14 15:33:01 +0100

    init

--- Triumph-Wall_Project/Assets/Scripts/Edificios_Scripts/CentroDeRetencion.cs
+++ Triumph-Wall_Project/Assets/Scripts/Edificios_Scripts/CentroDeRetencion.cs
@@ -8,6 +8,13 @@ public class CentroDeRetencion : Edificio
 {
 
 	private CR_Data myData;
+	private float salubridad;
+
+	private float factorSuciedad = 1.0f;
+
+	private int inmigrantesPorGuardia = 5;
+
+	private List<Edificio> edificiosDelRecinto = new List<Edificio>();
 
 	public override void Repair ( )
 	{
@@ -17,6 +24,16 @@ public class CentroDeRetencion : Edificio
 	public override void SetUP ( )
 	{
 		myData = ScriptableObject.CreateInstance<CR_Data>();
+
+		useUpgrades = false;
+		usedurability = false;
+		useEmployee = false; // llas funciones de los empleados se usaran desde el editor de rutas
+		useProcess = false ;
+		useInmigrants = true;//inmigrantes salubridad y control
+		//TODO initialize buidlings
+
+		maxInmigrantNum = CalculateMaxIlegalsInFacility();
+		currentInmigrantNum = GetCurrentTotalOfIlegals();
 	}
 
 	//after tick
@@ -34,6 +51,11 @@ public class CentroDeRetencion : Edificio
 
 	public override void Tick ( )
 	{
+		foreach ( Edificio building in edificiosDelRecinto)
+		{
+			building.Tick();
+		}
+		salubridad = CalculateSalubrity();
 		UpdateUIData();
 	}
 
@@ -45,5 +67,66 @@ public class CentroDeRetencion : Edificio
 	protected override void StartProcessInmigrant ( )
 	{
 		throw new System.NotImplementedException();
+	}
+	/// <summary>
+	/// En vez de calcular la salubridad desde aqui podriamos hacerlo desde los dormitorios
+	/// ya que solo afectan a la salubridad los inmigrantes de los dormitorios
+	/// </summary>
+	/// <returns></returns>
+	#region Salubrity
+
+	private float CalculateSalubrity ( )
+	{
+		float inmigrantesEnDormitorios = GetNumOfInmigrantsInDorms();
+		float inmigrantesMaxDormitorios = GetMaxOfInmigrantsInDorms();
+
+		float aforo = inmigrantesEnDormitorios / inmigrantesMaxDormitorios;
+		
+		//MIN -1 MAX 1
+		float result = 0;
+		if (aforo > 1)
+			result -= aforo * factorSuciedad * Time.deltaTime;
+		else
+			result += (1 + (1 - aforo * factorSuciedad * Time.deltaTime));
+
+		return result;
+	}
+
+	private float GetNumOfInmigrantsInDorms ( )
+	{
+		//TODO hacer dormitorios para poder completar esta funcion
+		return 10;
+	}
+	private float GetMaxOfInmigrantsInDorms ( )
+	{
+		//TODO hacer dormitorios para poder completar esta funcion
+		return 10;
+	}
+#endregion
+
+	private float CalculateControl ( )
+	{
+		float result = 0;
+		result = 1 - ((currentInmigrantNum - (currentEmployeeNum * inmigrantesPorGuardia)) / maxInmigrantNum);
+
+		return result;
+	}
+	private int CalculateMaxIlegalsInFacility ( )
+	{
+		int result = 0;
+		foreach (Edificio building in edificiosDelRecinto)
+		{
+			result += building.GetMaxInmigrants();
+		}
+		return result;
+	}
+	private int GetCurrentTotalOfIlegals ( )
+	{
+		int result = 0;
+		foreach (Edificio building in edificiosDelRecinto)
+		{
+			result += building.GetCurrentInmigrants();
+		}
+		return result;
 	}
 }
