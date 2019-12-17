315af63634f3ee2156068c37cb0722dd2f0091f0
Alex Gomez Cortes <gomezcortes10@gmail.com>
2019-12-17 18:39:45 +0100

    object factory wip

--- Triumph-Wall_Project/Assets/Scripts/Edificios_Scripts/OficinaDeportacionBehaviour.cs
+++ Triumph-Wall_Project/Assets/Scripts/Edificios_Scripts/OficinaDeportacionBehaviour.cs
@@ -108,4 +108,5 @@ public class OficinaDeportacionBehaviour : Edificio
     {
         currentEmployeeNum++;
     }
+
 }
---
+++ Triumph-Wall_Project/Assets/Scripts/ObjectFactory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFactory : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    Agent CreateAgent()
+    {
+        return new Agent();
+    }
+
+    /*Vehicle CreateVehicle()
+    {
+        return new Vehicle();
+    }*/
+    
+    /*Tower CreateTower()
+    {
+        return new Tower();
+    }*/
+}
