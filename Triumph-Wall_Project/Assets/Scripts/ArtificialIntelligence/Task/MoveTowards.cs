d8e2217f2375ea35f01db5ea2badbe9f6947ec3c
Abraham Armas Cordero <abraham.armas@enti.cat>
2020-01-06 21:40:55 +0100

    Move towards cutre

--- Triumph-Wall_Project/Assets/Scripts/ArtificialIntelligence/Task/MoveTowards.cs
+++ Triumph-Wall_Project/Assets/Scripts/ArtificialIntelligence/Task/MoveTowards.cs
@@ -1,9 +1,35 @@
 using UnityEngine;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine.AI;
 
 public class MoveTowards : Action
 {
     public TreeBlackBoard blackBoard;
+
+	private NavMeshAgent navA = null;
+	public override void OnStart ( )
+	{
+		base.OnStart();
+		Agent target = (Agent)blackBoard.Value.variables["Target"];
+		navA = this.GetComponent<NavMeshAgent>();
+		navA.SetDestination( target.gameObject.transform.position );
+
+	}
+	public override TaskStatus OnUpdate( )
+	{
+		if (!navA.pathPending)
+		{
+			if (navA.remainingDistance <= navA.stoppingDistance)
+			{
+				if (!navA.hasPath || navA.velocity.sqrMagnitude == 0f)
+				{
+					// Done
+					return TaskStatus.Success;
+				}
+			}
+		}
+		return TaskStatus.Running;
+	}
 
 }
